1d8e5fad9a69d06cffa75ba2f32d0f560dbc0fad
Eric <eric@appstream.com.my>
2018-05-02 19:05:21 +0800

    clean up files

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route , Switch } from 'react-router-dom';
+import { Router, Route , Switch , Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { history } from '../_helpers';
